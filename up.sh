f0edf54eeea737925ad6e3a41f6b1e8aa9cbdbcc
the-redback <maruf.2hin@gmail.com>
2019-11-25 01:24:26 +0600

    Loadbalancer using metalLB

--- up.sh
+++ up.sh
@@ -14,4 +14,19 @@ export KUBECONFIG=$HOME/.kube-vagrant/config
 echo "Installing rancher/local-path-provisioner for PVC"
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 
-
+# ref https://metallb.universe.tf/installation/
+echo "Installing MetalLB for loadBalancer"
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 192.168.205.240-192.168.205.250
+EOF
